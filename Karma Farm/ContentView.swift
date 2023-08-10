c3cae598046f684cad1d9af031cf617af62b9a2f
realtobyfu <fuz.sh0710@gmail.com>
2023-08-10 17:50:38 +0800

    Initial Commit

---
+++ Karma Farm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Karma Farm
+//
+//  Created by Tobias Fu on 8/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Karma Farm/Karma_FarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Karma_FarmApp.swift
+//  Karma Farm
+//
+//  Created by Tobias Fu on 8/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Karma_FarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
